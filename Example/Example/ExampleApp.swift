9b21f76c7382177a3bc9e19e933a225d6cfb0489
Guilherme Souza <guilherme@grds.dev>
2022-02-13 11:23:18 -0300

    Add example app

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Guilherme Souza on 13/02/22.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
